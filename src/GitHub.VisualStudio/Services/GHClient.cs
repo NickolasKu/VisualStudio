6172c5d6ecc5257e693ca552a004478e69d22a1d
Jamie Cansdale <jcansdale@github.com>
2018-03-05 19:23:24 +0000

    Extract GHClient into separate file

---
+++ src/GitHub.VisualStudio/Services/GHClient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using GitHub.Models;
+using Octokit;
+
+namespace GitHub.VisualStudio
+{
+    [Export(typeof(IGitHubClient))]
+    public class GHClient : GitHubClient
+    {
+        [ImportingConstructor]
+        public GHClient(IProgram program)
+            : base(program.ProductHeader)
+        {
+        }
+    }
+}
